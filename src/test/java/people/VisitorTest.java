dfa0bae9fd77a60f485da85a66f84093b4cc0cd0
Heather King <hkingdevelopment@gmail.com>
2022-01-10 18:04:04 +0000

    adds visitedAttractions array to Visitor adn a method to add attraction to the list

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,6 @@
 package people;
 
+import attractions.Dodgems;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +9,12 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    Dodgems dodgems;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        dodgems = new Dodgems("Bumper Cars", 5, 0);
     }
 
     @Test
@@ -27,5 +30,11 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void canAddAttractionToVisitedAttractionsList() {
+        visitor.visitedAttraction(dodgems);
+        assertEquals(1, visitor.visitedAttractionsCount());
     }
 }
---
+++ src/test/java/themepark/ThemeParkTest.java
@@ -0,0 +1,14 @@
+package themepark;
+
+import org.junit.Before;
+import themePark.ThemePark;
+
+public class ThemeParkTest {
+
+//    ThemePark themePark;
+//
+//    @Before
+//    public void before() {
+//        themePark = new ThemePark()
+//    }
+}
